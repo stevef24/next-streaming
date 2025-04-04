acb1887accff30fbc8ebd83068c5d9cbf90695df
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2025-04-04 17:15:33 +0100

    Refactor DisplayCards to use Suspense for loading product cards; introduced DisplayCard component for better structure and added unique keys with uuid.

--- components/demo/DisplayCards.tsx
+++ components/demo/DisplayCards.tsx
@@ -1,15 +1,26 @@
+import { Suspense, use } from "react";
 import { getProducts } from "@/lib/getCards";
+import { v4 as uuidv4 } from "uuid";
 
 import InfoCard from "@/components/demo/InfoCard";
+import { CardProps } from "@/types";
+import CardSkeleton from "./CardSkeleton";
+
+const DisplayCard = ({ cardPromise }: { cardPromise: Promise<CardProps> }) => {
+	const { name, price } = use(cardPromise);
+	return <InfoCard key={name} name={name} price={price} />;
+};
+
 const DisplayCards = async () => {
 	const cards = await getProducts();
-	const cardsWithData = await Promise.all(cards);
 
 	return (
 		<div className="w-full max-w-4xl mx-auto px-4 sm:px-6">
 			<div className="grid grid-cols-1 sm:grid-cols-2 grid-rows-2 gap-6 py-6">
-				{cardsWithData.map((card) => (
-					<InfoCard key={card.name} name={card.name} price={card.price} />
+				{cards.map((card) => (
+					<Suspense fallback={<CardSkeleton />} key={uuidv4()}>
+						<DisplayCard cardPromise={card} />
+					</Suspense>
 				))}
 			</div>
 		</div>
