7684c5e74e14a58c36e4d0105b1cf05343d5d8d2
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2025-04-06 12:51:50 +0100

    Update import path for DisplayCards component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import DisplayCards from "@/components/demo/dashboard";
+import DisplayCards from "@/components/demo/DisplayCards";
 export default function Home() {
 	return (
 		<div className="w-full h-screen flex justify-center items-center">
